2d1b7d883f731609efedf166f9c2e6b30cbb5f4b
pyncz <pavelyanycz@gmail.com>
2023-02-28 00:56:27 +0400

    fix: Update layout's `sidebar` height and position

--- src/layouts/LayoutSide.tsx
+++ src/layouts/LayoutSide.tsx
@@ -8,8 +8,8 @@ type Props = WithClassName<WithOptionalSlots<'details'>>
  */
 export const LayoutSide: FC<PropsWithChildren<Props>> = ({ children, details, className }) => {
   return (
-    <div className={`md:tw-grid md:tw-grid-cols-[3fr_minmax(18rem,1fr)] ${className ?? ''}`}>
-      <aside className="tw-border-b tw-border-b-separator md:tw-overflow-y-auto md:tw-col-[2] md:tw-border-b-0 md:tw-border-l md:tw-border-l-separator">
+    <div className={`tw-h-full md:tw-grid md:tw-grid-cols-[3fr_minmax(18rem,1fr)] ${className ?? ''}`}>
+      <aside className="tw-border-b tw-border-b-separator md:tw-max-h-screen md:tw-sticky md:tw-top-0 md:tw-overflow-y-auto md:tw-col-[2] md:tw-border-b-0 md:tw-border-l md:tw-border-l-separator">
         {details ?? null}
       </aside>
 
