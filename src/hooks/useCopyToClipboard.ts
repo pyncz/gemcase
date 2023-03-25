b90b9574120226ffe818bf5dfa9e76d530631d4b
pyncz <pavelyanycz@gmail.com>
2023-03-25 16:39:23 +0400

    fix: Add `preventDefault` on CopyButton click in order to be able to use it inside links

--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -1,9 +1,12 @@
+import type { MouseEvent } from 'react'
 import { useResetableState } from './useResetableState'
 
 export const useCopyToClipboard = (value: string) => {
   const [justCopied, setJustCopied] = useResetableState(false, 2000)
 
-  const copy = async () => {
+  const copy = async (e?: MouseEvent) => {
+    e?.preventDefault()
+
     await navigator.clipboard.writeText(value)
     setJustCopied(true)
   }
